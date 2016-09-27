1bc8139b92787d0a9f99074bd3d2860a4221fc8e
Paul <mackp@uchicago.edu>
2016-09-27 14:14:36 -0500

    adding a script to download a geojson file from the Plenario API

--- plenario_spatial_example.py
+++ plenario_spatial_example.py
@@ -1,8 +1,13 @@
+#download the 311 requests for tree trims from the beginning of the year until the present day and save it as a geojson file
 import requests
 import json
-trees  = "http://plenar.io/v1/api/grid/?obs_date__ge=2014-01-01&obs_date__le=2014-12-31&dataset_name=311_service_requests_tree_trims"
+import datetime
 
+year_begin = str(datetime.datetime.now().year) + "-01-01"
+current_date = str(datetime.datetime.now().year) + "-" + str(datetime.datetime.now().month) + "-" + str(datetime.datetime.now().day)
 
+# request is a call to the plenario api to retrieve
+trees  = "http://plenar.io/v1/api/grid/?obs_date__ge=" + year_begin + "&obs_date__le=" + current_date + "&dataset_name=311_service_requests_tree_trims"
 
 r = requests.get(trees)
 json_out = r.text
@@ -10,8 +15,3 @@ print(r.text)
 with open('mygeojson', 'w') as f:
     f.write(r.text)
 f.close()
-# def web_call(url, xml):
-#   r = requests.get(url)
-#   rtext = r.text
-#   web_reponse = bs4.BeautifulSoup(rtext,"xml")
-#   return web_reponse
