7a9b20d2bf1d1a800bf680751cf5ec51e8b5b6b7
Paul <mackp@uchicago.edu>
2016-10-07 13:23:48 -0500

    clean up data generation file

--- pothole.py
+++ pothole.py
@@ -1,9 +1,9 @@
-#download the 311 requests for potholes from the beginning of the year until the present day and save it as a geojson file. Filter so only completed potholes are counted
 import requests
 import json
 import datetime
 import ast
 
+# generate dynamic date times
 year_begin = str(datetime.datetime.now().year) + "-01-01"
 current_date = str(datetime.datetime.now().year) + "-" + str(datetime.datetime.now().month) + "-" + str(datetime.datetime.now().day)
 
@@ -13,7 +13,7 @@ r = requests.get(census)
 population = r.text
 population = ast.literal_eval(population)
 
-# call the plenario api to get the relevant data per neighborhood
+# call the plenario api to get the relevant data per zipcode
 potholes  = "http://plenar.io/v1/api/shapes/boundaries_zip_codes/311_service_requests_pot_holes_reported?obs_date__ge=" + year_begin + "&obs_date__le=" + current_date + '&311_service_requests_pot_holes_reported__filter={"op":"eq", "col":"status", "val":"Completed"}'
 r = requests.get(potholes)
 json_out = r.text
@@ -32,30 +32,4 @@ for z1 in output['features']:
 json_string = json.dumps(output)
 with open('potholes.json', 'w') as f:
 	f.write(json_string)
-f.close()
-
-
-# find the range of the counts and determine appropriate quartiles
-json = json.loads(json_string)
-ouput_range = set([])
-for i in json['features']:
-	ouput_range.add(i['properties']['pothole_per_1000_ppl'])
-top = max(ouput_range)
-bottom = min(ouput_range)
-q1 = (top - bottom) * (1/7)
-q2 = (top - bottom) * (2/7)
-q3 = (top - bottom) * (3/7)
-q4 = (top - bottom) * (4/7)
-q5 = (top - bottom) * (5/7)
-q6 = (top - bottom) * (6/7)
-
-
-with open('pothole_quantiles.txt', 'w') as f:
-	f.write('q1:' + str(min(ouput_range))  + ' - ' + str(q1) + '\n')
-	f.write('q2:' + str(q1) + '- ' + str(q2) +  '\n')
-	f.write('q3:' + str(q2) + '- ' + str(q3) + '\n')
-	f.write('q4:' + str(q3) + '- ' + str(q4) + '\n')
-	f.write('q5:' + str(q4) + '- ' + str(q5) + '\n')
-	f.write('q6:' + str(q5) + '- ' + str(q6) + '\n')
-	f.write('q7:' + str(q6) + '- ' + str(max(ouput_range)) + '\n')
 f.close()
